eb39a5937936e2b264e8cf05dffbf167c9bfb3f0
PilouFR <pl.duthoit@gmail.com>
2016-03-14 15:14:36 +0100

    Styling the website

--- rssDisplayer.js
+++ rssDisplayer.js
@@ -1,95 +1,153 @@
+/**
+ * Globals
+ */
+
+var podcastList=new Array();
+/**
+ * Init function.
+ */
 function init() {
-	loadChapter(); // charge un premier chapitre
-	window.addEventListener("hashchange",loadChapter); // charge un chapitre à chaque fois que l'url change
-}
+	document.getElementById("createButton").addEventListener("click",createAlarm);
+	
+	for(var index = 0; index < soundList.length; index++) {
+		var s=soundList[index];
+	}
 
-function getXMLHttpRequest() { // renvoit un objet XMLHttpRequest valide
-	var xhr = null;
-	if (window.XMLHttpRequest || window.ActiveXObject) {
-		if (window.ActiveXObject) {
-			try {
-				xhr = new ActiveXObject("Msxml2.XMLHTTP");
-			} catch(e) {
-				xhr = new ActiveXObject("Microsoft.XMLHTTP");
-			}
-		} else {
-			xhr = new XMLHttpRequest(); 
+
+	console.log("onload -> init() successful");
+}
+/**
+ * Sceondary functions.
+ */
+function createPodcast() {
+	var ID=podcastList.length;
+	var holder=document.getElementById("alarmHolder");
+	var newPodcast = {
+		id:ID,
+		pcTitle:
+		pcLink:
+		pcDesc:
+		pcImage:
+
+		li:document.createElement("li"),
+		CBactive:document.createElement("input"),
+		NumberHour:document.createElement("input"),
+		NumberMinute:document.createElement("input"),
+		TextName:document.createElement("input"),
+		SelectSound:document.createElement("select"),
+		ButtonDelete:document.createElement("input"),
+		Audio:document.createElement("audio")
+
+
+	}
+	newPodcast.li.name="alarm";
+	newPodcast.li.className="alarm";
+
+	newPodcast.CBactive.type="checkbox";
+	newPodcast.CBactive.name="active";
+	newPodcast.CBactive.className="checkbox";
+	newPodcast.CBactive.addEventListener("click",function(){stopAlarm(newPodcast)});;
+	newPodcast.li.appendChild(newPodcast.CBactive);
+
+	newPodcast.NumberHour.type="number";
+	newPodcast.NumberHour.name="hour";
+	newPodcast.NumberHour.className="number";
+	newPodcast.NumberHour.max=23;
+	newPodcast.NumberHour.min=0;
+	newPodcast.NumberHour.value=0;
+	newPodcast.li.appendChild(newPodcast.NumberHour);
+
+	newPodcast.NumberMinute.type="number";
+	newPodcast.NumberMinute.name="minute";
+	newPodcast.NumberMinute.className="number";
+	newPodcast.NumberMinute.max=59;
+	newPodcast.NumberMinute.min=0;
+	newPodcast.NumberMinute.value=0;
+	newPodcast.li.appendChild(newPodcast.NumberMinute);
+
+	newPodcast.TextName.type="text";
+	newPodcast.TextName.name="name";
+	newPodcast.TextName.className="name";
+	newPodcast.TextName.value="Nouvelle alarme "+ID;
+	newPodcast.li.appendChild(newPodcast.TextName);
+
+	newPodcast.SelectSound.name="sound";
+	for(var index = 0; index < soundList.length; index++) {
+		var s=soundList[index];
+		var selected="";
+		if(s.selected)
+			selected=" selected";
+		newPodcast.SelectSound.innerHTML+='<option value="'+s.path+'" '+selected+'>'+s.name+'</option>';
+	}
+	newPodcast.SelectSound.addEventListener("change",function() {
+		var current=newPodcast.Audio.src;
+		var selected=newPodcast.SelectSound.options[newPodcast.SelectSound.selectedIndex].value;
+		if(current!==selected) {
+			newPodcast.Audio.pause();
+			newPodcast.Audio.src=selected;
+			newPodcast.Audio.load();
 		}
-	} else {
-		alert("Votre navigateur ne supporte pas l'objet XMLHTTPRequest...");
-		return null;
-	}
-	return xhr;
-}
-function changeChapter(cptr) { // Lorsque qu'un chapitre est récupéré depuis le serveur, l'affiche.
-	// transformation de la requête en objet
-	var j=JSON.parse(cptr); 
-	
-	// récupération des données
-	var chapterText=j.txt;
-	var chapterLinks=j.links;
-	
-	// on affiche le nouveau text.
-	document.getElementById("chapterText").innerHTML=chapterText;
-	
-	var links=document.getElementById("chapterLinks");
-	
-	// on suppriemtous les liens existants.
-	while(links.firstChild)
-		links.removeChild(links.firstChild);
-	
-	// on créé les nouveaux liens
-	for(var i=0;i<chapterLinks.length;i++) {
-		var li=document.createElement("li");
-		li.className="link";
-		var a=document.createElement("a");
-		a.href=chapterLinks[i].link;
-		li.innerHTML=chapterLinks[i].txt;
-		a.appendChild(li);
-		links.appendChild(a);
-	}
-}
-function AJAXretrieveRequest(req) { // lorsque le status change, vérifie si on a reçu un chapitre.
-	if (req.readyState === 4) {
-		if(req.status === 200) { // répose valide.
-			console.log("AJAX valid answer.");
-			changeChapter(req.responseText); // changement de chapitre avec les nouvelels données.
-		} else console.log("AJAX invalid answer: "+req.status); // réponse invalide.
-	}
-}
-function AJAXsendRequest(xhr,target) { // envoit la requête AJAX
-	// prépapre la récupératino du chapitre dont le numéros est "target"
-	xhr.open("GET", "http://crossorigin.me/http://iutdoua-webetu.univ-lyon1.fr/~p1402828/S4-CWR/TP2/AJAX/chapitre"+target+".json");
-	
-	// envoyer requête
-	xhr.send();
-	console.log("AJAX request sent.");
-}
-function loadChapter() { // met en palce la récupération d'un chapitre
-	// création objet XMLHttpRequest
-	var xhr = getXMLHttpRequest();
-	
-	// vérification validité
-	if(xhr == null) {
-		console.log("Unable to create WMLHttpRequest object.");
-		return false;
-	}
-	
-	// préparation de la réception
-	xhr.onreadystatechange = function() {
-		AJAXretrieveRequest(xhr);
-	};
-	
-	// préparation de l'envoi
-	var target = window.location.hash.substring(1);
-	if(target=="")
-		target="1";
-	
-	// envoi
-	AJAXsendRequest(xhr,target);
-	
-	return true;
+		console.log("Selected sound changed");
+	});
+	newPodcast.li.appendChild(newPodcast.SelectSound);
+
+	newPodcast.ButtonDelete.type="button";
+	newPodcast.ButtonDelete.name="delete";
+	newPodcast.ButtonDelete.className="button";
+	newPodcast.ButtonDelete.value="-";
+	newPodcast.ButtonDelete.addEventListener("click",function() {
+		deleteAlarm(newPodcast);
+	});
+	newPodcast.li.appendChild(newPodcast.ButtonDelete);
+
+	newPodcast.Audio.src=newPodcast.SelectSound.options[newPodcast.SelectSound.selectedIndex];
+	newPodcast.Audio.load();
+	newPodcast.li.appendChild(newPodcast.Audio);
+
+	podcastList[ID]=newPodcast;
+
+	holder.appendChild(newPodcast.li);
+
+	console.log("onclick -> createAlarm() successful");
 }
 
-// lorsque le chargement de la page est terminé, on initialise.
-window.addEventListener("load",init);
+function deleteAlarm(alarm) {
+	stopAlarm(alarm);
+	var holder=document.getElementById("alarmHolder");
+	holder.removeChild(alarm.li);
+}
+/**
+ * Start the sound for the current alarm if not playing.
+ * Change the url if alarm choice changed.
+ * Change style.
+ */
+function startAlarm(alarm) {
+	if(alarm.Audio.paused===true) {
+		alarm.Audio.play();
+		alarm.li.className="alarmActive";
+		console.log("startAlarm() successful");
+		console.log(alarm.Audio.src);
+		console.log("Play -> is paused: "+alarm.Audio.paused);
+	}
+}
+/**
+ * Stop sound.
+ * Change style.
+ */
+function stopAlarm(alarm) {
+	if(alarm.Audio) {
+		alarm.Audio.pause();
+	}
+	alarm.li.className="alarm";
+}
+/**
+ * Other functions
+ */
+ function toStr(numeric) {
+	 if(numeric<10)
+		 return "0"+numeric;
+	 else
+		return ""+numeric;
+ }
+
+window.addEventListener("load", init);
