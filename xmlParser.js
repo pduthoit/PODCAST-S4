71dcce88acf598889650a24b4c1e38fd88f92ebe
Dare <zeratul_du_38@hotmail.fr>
2016-03-16 15:15:18 +0100

    xmlParser
    Done with it. I think.

--- xmlParser.js
+++ xmlParser.js
@@ -2,7 +2,7 @@ function init() {
 	
 }
 
-function getXMLHttpRequest() { // renvoit un objet XMLHttpRequest valide
+function getXMLHttpRequest() { // return a valid XMLHttpRequest
 	var xhr = null;
 	if (window.XMLHttpRequest || window.ActiveXObject) {
 		if (window.ActiveXObject) {
@@ -15,7 +15,7 @@ function getXMLHttpRequest() { // renvoit un objet XMLHttpRequest valide
 			xhr = new XMLHttpRequest(); 
 		}
 	} else {
-		alert("Votre navigateur ne supporte pas l'objet XMLHTTPRequest...");
+		alert("Your browser does not support XMLHTTPRequest object...");
 		return null;
 	}
 	return xhr;
@@ -41,14 +41,14 @@ function createItemData(item) {
  * Create an Array with all the data of one Channel.
  * Use it to create a new podcast.
  */
-function getChannelData(rss) { // Lorsque qu'un podcast est récupéré, on l'affiche.
-	// transformation de la requête en objet XML
+function getChannelData(rss) { // Retrieve data from a xml file.
+	// convert to XML object.
 	var xml=rss.responseXML; 
 	
-	// récupération des données
+	// preparing data holder
 	var channels=xml.getElementsByTagName("channel");
 	var datas;
-	// pour chaque channels:
+	// for each channels
 	for(var i=0; i<channels.length;i++) {
 		datas=new Array("title","link","description",0);
 		datas[3]=new Array();
@@ -58,53 +58,48 @@ function getChannelData(rss) { // Lorsque qu'un podcast est récupéré, on l'af
 			e=elemC[j];
 			if(e.tagName==datas[0]) // if no title yet
 				datas[0]=e.childNodes[0].nodeValue;
-			if(e.tagName==datas[1]) // if no ling yet
+			if(e.tagName==datas[1]) // if no link yet
 				datas[1]=e.childNodes[0].nodeValue;
 			if(e.tagName==datas[2]) // if no descriptino yet
 				datas[2]=e.childNodes[0].nodeValue;
 			if(e.tagName=="item")
 				datas[3][datas[3].length]=getItemData(e);
 		}
-		createPodcast(datas[0],datas[1],datas[2],datas[3]); // création d'un noveau Podcast
+		createPodcast(datas[0],datas[1],datas[2],datas[3]); // create new podcast
 	}
 }
-function XMLretrieveRequest(req) { // lorsque le status change, vérifie si on a reçu un XML.
+function XMLretrieveRequest(req) { // when status change, load new podacst
 	if (req.readyState === 4) {
-		if(req.status === 200) { // répose valide.
+		if(req.status === 200) { // valid answer.
 			console.log("AJAX valid answer.");
-			getChannelData(req); // chargement d'un nouveau podcast.
-		} else console.log("AJAX invalid answer: "+req.status); // réponse invalide.
+			getChannelData(req); // loading new podcast
+		} else console.log("AJAX invalid answer: "+req.status); // invalid answer
 	}
 }
-function AJAXsendRequest(xhr,target) { // envoit la requête AJAX
-	// prépapre la récupératino du XML à l'adresse "target"
-	xhr.open("GET",target);
+function AJAXsendRequest(xhr,target) { // send AJAX request
+	// preparing cross domain request for the targeted url.
+	xhr.open("GET","http://crossorigin.me/"+target);
 	
-	// envoit requête
+	// send request
 	xhr.send();
 	console.log("AJAX request sent.");
 }
-function loadPodcast() { // met en place la récupération d'un podcast
-	// création objet XMLHttpRequest
+function loadPodcast(target) { // preapre podcast request
+	// create XMLHttpRequest object
 	var xhr = getXMLHttpRequest();
 	
-	// vérification validité
+	// check for validity
 	if(xhr == null) {
 		console.log("Unable to create XMLHttpRequest object.");
 		return false;
 	}
 	
-	// préparation de la réception
+	// prepare for answer
 	xhr.onreadystatechange = function() {
 		XMLretrieveRequest(xhr);
 	};
 	
-	// préparation de l'envoi
-	var target = window.location.hash.substring(1);
-	if(target=="")
-		target="1";
-	
-	// envoi
+	// sending
 	AJAXsendRequest(xhr,target);
 	
 	return true;
